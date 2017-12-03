196415842f854ae80406c207fe75fddb2cb35029
Michael R Raigosa <mraigosa1965@gmail.com>
2017-12-02 23:43:51 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "QUKRyWnAfaN3uZcpeeq9Km5Gp"
+consumer_secret = "OzqXAascwDZRerQ7z40Cr3mRLYaITRv3H3sy9gFVWS7l8NRQaK"
+access_token = "930594415369949184-bZkOzmFb9nyfrvN7Sxit8kzuF2T8J46"
+access_token_secret = "VnWoxd7CtjKh0On06U03tvR2gj5BObY14jUh32xzvn2Cw"
